8a2ea253abc22bc7fcc7af94b8f686f3c754c14f
Steven Maloney <steven.amloney@gmail.com>
2020-12-18 14:25:14 -0600

    page routes to details on click of poster

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -6,12 +6,17 @@ class List extends Component {
         this.props.dispatch({type: 'FETCH_POSTERS'});
     }
 
+    handleClick(film) {
+        this.props.dispatch({type: 'FETCH_DETAILS', payload: film})
+        this.props.history.push('/details');
+    }
+
     render() {
         return(
             <div>
                 <ul>
                     {this.props.reduxState.movies.map(film => 
-                    <li key={film.id}>{film.title} <img src={film.poster}></img></li>)}
+                    <li key={film.id}>{film.title} <img src={film.poster} alt="movie poster" onClick={(event)=> this.handleClick(film)}></img></li>)}
                 </ul>
             </div>
         )
