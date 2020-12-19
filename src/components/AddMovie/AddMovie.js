ddf086a5a1c85ec9f36db2bb8fad2ee112b8ec37
sdeda1us <steven.amloney@gmail.com>
2020-12-19 15:06:59 -0600

    added base styling for addMovie component

--- src/components/AddMovie/AddMovie.js
+++ src/components/AddMovie/AddMovie.js
@@ -22,37 +22,50 @@ class AddMovie extends Component {
         return(
             <div>
                 <h3>Add a movie</h3>
+                
                 <form onSubmit={this.handleSubmit}>
-                    <label>Movie Name</label>   
-                    <input type="text" onChange={(event) => this.props.dispatch({type:'SET_TITLE', payload: event.target.value})} />
-                    <label>Movie Poster URL</label>
-                    <input type="text" onChange={(event) => this.props.dispatch({type:'SET_URL', payload: event.target.value})}/>
-                    <label>Movie Description</label>
-                    <br/>
-                    <textarea type="text" rows="6" columns="100" onChange={(event) => this.props.dispatch({type:'SET_DESCRIPTION', payload: event.target.value})}/>
-                    <br/>
+                    <div className="flexContainer">
+                    <div className="formSpace">
+                        <div className="textForm">
+                            <label>Movie Name: </label>   
+                            <input className="inputStyle" type="text" onChange={(event) => this.props.dispatch({type:'SET_TITLE', payload: event.target.value})} />
+                        </div>
+                        <div className="textForm">
+                            <label>Movie Poster URL: </label>
+                            <input type="text" onChange={(event) => this.props.dispatch({type:'SET_URL', payload: event.target.value})}/>
+                        </div>
+                        <div className="textForm">
+                            <label>Movie Description: </label>
+                        </div>
+                        <textarea type="text" rows="6" cols="50" onChange={(event) => this.props.dispatch({type:'SET_DESCRIPTION', payload: event.target.value})}/>
+                    </div>
+                    <div clasName="formSpace">
                     <label>Genre</label>
-                    <select name="genres" onChange={(event) => this.props.dispatch({type:'SET_GENRE', payload: event.target.value})}>
-                        <option>Choose a genre</option>
-                        <option value="1">Adventure</option>
-                        <option value="2">Animated</option>
-                        <option value="3">Biographical</option>
-                        <option value="4">Comedy</option>
-                        <option value="5">Disaster</option>
-                        <option value="6">Drama</option>
-                        <option value="7">Epic</option>
-                        <option value="8">Fantasy</option>
-                        <option value="9">Musical</option>
-                        <option value="10">Romantic</option>
-                        <option value="11">Science Fiction</option>
-                        <option value="12">Space-Opera</option>
-                        <option value="13">Superhero</option>
-                    </select>
+                        <select name="genres" onChange={(event) => this.props.dispatch({type:'SET_GENRE', payload: event.target.value})}>
+                            <option>Choose a genre</option>
+                            <option value="1">Adventure</option>
+                            <option value="2">Animated</option>
+                            <option value="3">Biographical</option>
+                            <option value="4">Comedy</option>
+                            <option value="5">Disaster</option>
+                            <option value="6">Drama</option>
+                            <option value="7">Epic</option>
+                            <option value="8">Fantasy</option>
+                            <option value="9">Musical</option>
+                            <option value="10">Romantic</option>
+                            <option value="11">Science Fiction</option>
+                            <option value="12">Space-Opera</option>
+                            <option value="13">Superhero</option>
+                        </select>
+                    </div>
+                    </div>
+                    <div>
                     <button type="submit">Save</button>
                     <button onClick={(event)=> {this.props.dispatch({type: 'CLEAR_FORM_DATA'}); 
                                                 this.props.history.push('/')}}>
                                                 Cancel
                                                 </button>
+                    </div>
                 </form>
             </div>
         )
