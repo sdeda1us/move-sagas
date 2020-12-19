2cba341e5cecf3d20193f85375052ee95f9f9b9d
sdeda1us <steven.amloney@gmail.com>
2020-12-19 14:24:50 -0600

    details page with more styling

--- src/components/Details/Details.js
+++ src/components/Details/Details.js
@@ -8,13 +8,18 @@ class Details extends Component {
         return(
             <div>
                 <h2>{this.props.reduxState.movieToDetail.title}</h2>
-                <img src={this.props.reduxState.movieToDetail.poster}/>
-                <p>Genres</p>
-                <ul>
-                    {this.props.reduxState.genres.map(type => <li>{type.name}</li>)}
-                </ul>
-                <p>Details: {this.props.reduxState.movieToDetail.description}</p>
-                <button onClick={(event) => this.props.history.push('/')}>Back to List</button>
+                <div className="detailSpace">
+                    <img src={this.props.reduxState.movieToDetail.poster}/>
+                    <div>
+                    <p>Genres</p>
+                        <ul>
+                            {this.props.reduxState.genres.map(type => <li>{type.name}</li>)}
+                        </ul>
+                    </div>
+                    <p>Details: {this.props.reduxState.movieToDetail.description}</p>
+                    <br/>
+                    <button className="returnButton" onClick={(event) => this.props.history.push('/')}>Back to List</button>
+                </div>
             </div>
         )
     }
