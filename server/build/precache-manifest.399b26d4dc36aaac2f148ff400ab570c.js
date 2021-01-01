1a1b4e2cd49dae07fb9e11b648e5b452bc9d2316
sdeda1us <steven.amloney@gmail.com>
2020-12-31 23:25:37 -0600

    trying amazon deploy

---
+++ server/build/precache-manifest.399b26d4dc36aaac2f148ff400ab570c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "978598a9d05733baef9e53b2bd591cf6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb2e7a10750362690e20",
+    "url": "/static/css/main.c15c048f.chunk.css"
+  },
+  {
+    "revision": "1ac2bab9ad9c5f95db97",
+    "url": "/static/js/2.33e82617.chunk.js"
+  },
+  {
+    "revision": "ff2a223b51b86718de583271daa0b126",
+    "url": "/static/js/2.33e82617.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb2e7a10750362690e20",
+    "url": "/static/js/main.e85b423f.chunk.js"
+  },
+  {
+    "revision": "800c06ce6c2e383ac1fe",
+    "url": "/static/js/runtime-main.52009e65.js"
+  },
+  {
+    "revision": "165950f435e934eb518f9e7c7f0b5d97",
+    "url": "/static/media/movie_seats.165950f4.jpg"
+  }
+]);
--- server/server.js
+++ server/server.js
@@ -4,14 +4,19 @@ const bodyParser = require('body-parser');
 const movieRouter = require('./routes/movie.router.js')
 const genreRouter = require('./routes/genre.router.js')
 const port = process.env.PORT || 5000;
+const path = require('path');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for angular requests
-app.use(express.static('build'));
+app.use(express.static(path.join(__dirname, '/build')));
 
 /** ---------- ROUTES ---------- **/
 app.use('/api/movie', movieRouter);
 app.use('/api/genre', genreRouter)
+
+app.get('*', (req, res)=>{
+    res.sendFile(path.join(__dirname + '/build/index.html'));
+})
 
 /** ---------- START SERVER ---------- **/
 app.listen(port, function () {
